77f1d1c3d20cab76e352d62b4b506c38657b254f
Iaroslav Rykhalskyi <rikhalskiy@gmail.com>
2025-11-07 21:33:35 +0100

    Make border draggable

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from "./topbar/topbar.component";
 import { TopicListComponent } from './topic-list/topic-list.component';
@@ -13,4 +13,31 @@ import { SelectedTopicComponent } from './selected-topic/selected-topic.componen
 })
 export class AppComponent {
   title = 'ragatouille';
+
+  isDragging = false;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  startDragging(event: MouseEvent) {
+    this.isDragging = true;
+    event.preventDefault();
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onDrag(event: MouseEvent) {
+    if (this.isDragging) {
+      const container = this.el.nativeElement.querySelector('.main-container');
+      if (container) {
+        const containerRect = container.getBoundingClientRect();
+        const newWidth = event.clientX - containerRect.left;
+        const newWidthPercent = (newWidth / containerRect.width) * 100;
+        (this.el.nativeElement as HTMLElement).style.setProperty('--topic-list-width', `${newWidthPercent}%`);
+      }
+    }
+  }
+
+  @HostListener('document:mouseup')
+  stopDragging() {
+    this.isDragging = false;
+  }
 }
